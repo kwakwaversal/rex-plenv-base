156c4f8fdc92a7ed3277d0aab65df56f04f615b0
Paul Williams, Laptop <kwakwaversal@gmail.com>
2015-11-05 12:02:00 +0000

    Preparing plenv base package to do something useful.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# rex-plenv-base
-Rex module to install plenv
+# Rex::Plenv::Base
+
+A Rex module to install plenv (perl binary manager) and cpanm.
+
+# USAGE
+
+```perl
+include qw/Rex::Rex::Base/;
+
+task setup => make {
+  Rex::Rex::Base::setup();
+};
+```
+
+# See also
 
 http://www.rexify.org/docs/rex_book/infrastructure/example_of_a_complete_rex_code_infrastructure.html
---
+++ __module__.pm
@@ -0,0 +1,91 @@
+#
+# AUTHOR: Paul Williams <kwakwa@cpan.org>
+# REQUIRES: build-essential, curl, git
+# LICENSE: Apache License 2.0
+#
+# A Rex module to install plenv and cpanm on your Server.
+
+package Rex::Plenv::Base;
+
+use Rex -base;
+use Rex::Ext::ParamLookup;
+
+our %version_map = (
+  debian => "5.20.2",
+  ubuntu => "5.20.2",
+  centos => "5.20.2",
+  redhat => "5.20.2",
+);
+
+task setup => make {
+  my $perl_version = param_lookup "perl_version",
+    $version_map{ lc get_operating_system };
+
+  for my $pkg ([qw/build-essential curl git/]) {
+    pkg $pkg, ensure => "latest";
+  }
+
+  # Commands taken from
+  #
+  # http://weblog.bulknews.net/post/58079418600/plenv-alternative-for-perlbrew
+  # https://github.com/tokuhirom/plenv#readme
+
+  # Check out plenv into ~/.plenv
+  run 'ls ~/.plenv';
+  if ($? == 0) {
+    Rex::Logger::info "plenv has already been installed";
+  }
+  else {
+    run 'git clone git://github.com/tokuhirom/plenv.git ~/.plenv';
+  }
+
+  # Add ~/.plenv/bin to your $PATH for access to the plenv command-line utility.
+  # Add plenv init to your shell to enable shims and autocompletion.
+  run 'touch ~/.bash_profile';
+  append_if_no_such_line '~/.bash_profile', 'export PATH="$HOME/.plenv/bin:$PATH"';
+  append_if_no_such_line '~/.bash_profile', 'eval "$(plenv init -)"';
+
+  # Install perl-build, which provides an plenv install command that simplifies
+  # the process of installing new Perl versions.
+  run 'ls ~/.plenv/plugins/perl-build/';
+  if ($? == 0) {
+    Rex::Logger::info "perl-build has already been installed";
+  }
+  else {
+    run 'git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/';
+  }
+
+  # Additional commands to make plenv more perlbrew-like
+  # run 'git clone git://github.com/miyagawa/plenv-contrib.git ~/.plenv/plugins/plenv-contrib/';
+
+  # Add ~/plenv/bin to the Rex $PATH so it will be used for plenv commands
+  my @new_path;
+  push(@new_path, '$HOME/.plenv/bin');
+  push(@new_path, Rex::Config->get_path);
+  Rex::Config->set_path(\@new_path);
+
+  # Install perl!
+  run "plenv versions |grep $perl_version";    # this is probably a bug
+  if ($? == 0) {
+    Rex::Logger::info "perl-$perl_version has already been installed";
+  }
+  else {
+    run "plenv install $perl_version";
+  }
+
+  # Because all perl-based scripts (prove, plackup etc.) are also wrapped as
+  # shims, you have to run plenv rehash after installing CPAN modules that have
+  # scripts. But if you run plenv install-cpanm to bootstrap cpanminus, it will
+  # be automatically taken care of.
+  run "plenv rehash";
+
+  run "PLENV_VERSION=$perl_version plenv which cpanm";
+  if ($? == 0) {
+    Rex::Logger::info "cpanm has already installed for perl-$perl_version";
+  }
+  else {
+    run "PLENV_VERSION=$perl_version plenv install-cpanm";
+  }
+};
+
+1;
